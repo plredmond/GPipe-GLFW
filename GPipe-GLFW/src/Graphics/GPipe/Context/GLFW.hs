c63680c1317d334b4a63c8883c44fe6b28cf41cb
PLR <51248199+plredmond@users.noreply.github.com>
2015-06-29 21:18:07 -0700

    completed something which *could* work, but the types aren't working out

--- GPipe-GLFW/src/Graphics/GPipe/Context/GLFW.hs
+++ GPipe-GLFW/src/Graphics/GPipe/Context/GLFW.hs
@@ -1,64 +1,120 @@
-{-# LANGUAGE PackageImports #-}
 module Graphics.GPipe.Context.GLFW where
 
 -- qualified
 import qualified Text.Printf as P
-import qualified Data.Maybe as M
-import qualified Control.Exception as Exc
-import qualified "GLFW-b" Graphics.UI.GLFW as GLFW
+-- import qualified Data.Maybe as M
+import qualified Control.Monad as M
+-- import qualified Control.Exception as Exc
+import qualified Control.Concurrent as C
+import qualified Graphics.GPipe.Context.GLFW.Internal as Internal
+
 -- unqualified
-import Control.Applicative ((<$>))
+-- import Control.Applicative ((<$>))
+import Graphics.GPipe.Format (ContextFormat)
+import Graphics.GPipe.Context (ContextFactory, ContextHandle(..))
+
+------------------------------------------------------------------------------
+-- Top level
+
+newContext :: ContextFactory c ds
+newContext contextFormat = do
+    msgC <- C.newChan
+    _ <- C.forkIO $ do
+        P.printf "Thread beginning\n"
+        -- TODO: inform these arguments with the contextFormat
+        Internal.withGL Nothing Nothing $ contextLoop msgC
+        P.printf "Thread ending\n"
+        return ()
+    return ContextHandle
+        { newSharedContext = undefined {- \cf -> do
+            (NewSharedContextRet ch) <- await msgC (NewSharedContextMsg cf)
+            return ch -}
+        , contextDoSync = contextDoSync' msgC
+        , contextDoAsync = dispatch msgC . ContextDoAsyncMsg
+        , contextSwap = M.void $ await msgC ContextSwapMsg
+        , contextFrameBufferSize = do
+            (ContextFrameBufferSizeRet size) <- await msgC ContextSwapMsg
+            return size
+        , contextDelete = M.void $ await msgC ContextDeleteMsg
+        }
+
+contextDoSync' :: C.Chan (ContextFactoryMsg c ds a, C.Chan (ContextFactoryRet a))
+    -> IO a -> IO a
+contextDoSync' msgC action = do
+    (ContextDoSyncRet r) <- await msgC (ContextDoSyncMsg action)
+    return r
+
+contextLoop :: C.Chan (ContextFactoryMsg c ds a, C.Chan (ContextFactoryRet a))
+    -> Internal.Window
+    -> IO ()
+contextLoop msgC w = do
+    signal <- turnaround msgC $ contextStep w
+    M.when signal $ contextLoop msgC w
+
+contextStep :: Internal.Window -> ContextFactoryMsg c ds a -> IO (ContextFactoryRet a, Bool)
+contextStep w msg = do
+    P.printf "Context thread got a message\n"
+    case msg of
+        NewSharedContextMsg contextFormat -> do
+            -- TODO: create a shared context and reply with it
+            reply undefined
+        ContextDoSyncMsg action -> do
+            r <- action
+            reply undefined -- FIXME: reply with the result
+        ContextDoAsyncMsg action -> do
+            action
+            ignore
+        ContextSwapMsg -> do
+            -- TODO: swap buffers
+            ignore
+        ContextFrameBufferSizeMsg -> do
+            -- TODO: fetch size info
+            reply $ ContextFrameBufferSizeRet (10, 30)
+        ContextDeleteMsg -> stop
+    where
+        reply x = return (x, True)
+        ignore = return (OtherRet, True)
+        stop = return (OtherRet, False)
 
 ------------------------------------------------------------------------------
 -- Types & Constants
 
--- reexports
-type Window = GLFW.Window
-type ErrorCallback = GLFW.ErrorCallback
+-- Self-Addressed Envelope
+type SAE a b = C.Chan (a, C.Chan b)
 
--- a default error callback which ragequits
-defaultOnError :: GLFW.ErrorCallback
-defaultOnError err msg = fail $ P.printf "%s: %s" (show err) msg
+-- RPC and wait for result
+await :: SAE a b -> a -> IO b
+await msgC msg = do
+    replyC <- C.newChan
+    C.writeChan msgC (msg, replyC)
+    C.readChan replyC
 
--- initial window size & title suggestions
-data WindowConf = WindowConf
-    { width :: Int
-    , height :: Int
-    , title :: String 
-    }
+-- RPC and ignore result
+dispatch :: SAE a b -> a -> IO ()
+dispatch msgC msg = do
+    ignoredC <- C.newChan
+    C.writeChan msgC (msg, ignoredC)
 
-defaultWindowConf :: WindowConf
-defaultWindowConf = WindowConf 1024 768 "GLFW Window"
+-- Handle RPC
+turnaround :: SAE a b -> (a -> IO (b, c)) -> IO c
+turnaround msgC handler = do
+    (msg, replyC) <- C.readChan msgC
+    (reply, sig) <- handler msg
+    C.writeChan replyC reply
+    return sig
 
-------------------------------------------------------------------------------
--- Code
+data ContextFactoryMsg c ds a
+    = NewSharedContextMsg (ContextFormat c ds)
+    | ContextDoSyncMsg (IO a)
+    | ContextDoAsyncMsg (IO ())
+    | ContextSwapMsg
+    | ContextFrameBufferSizeMsg
+    | ContextDeleteMsg
 
--- set and unset the GLFW error callback, using a default if none is provided
-withErrorCallback :: Maybe GLFW.ErrorCallback -> IO a -> IO a
-withErrorCallback customOnError =
-    GLFW.setErrorCallback (Just $ M.fromMaybe defaultOnError customOnError)
-    `Exc.bracket_`
-    GLFW.setErrorCallback Nothing 
-
--- init and terminate GLFW
-withGLFW :: IO a -> IO a
-withGLFW = GLFW.init `Exc.bracket_` GLFW.terminate
-
--- create and destroy a new window, as the current context, using any monitor
-withNewWindow :: Maybe WindowConf -> (GLFW.Window -> IO a) -> IO a
-withNewWindow customWindowConf =
-    (M.fromMaybe noWindow <$> createWindowHuh)
-    `Exc.bracket`
-    GLFW.destroyWindow
-    where
-        noWindow = error "Couldn't create a window"
-        WindowConf w h t = M.fromMaybe defaultWindowConf customWindowConf
-        createWindowHuh = do
-            win <- GLFW.createWindow w h t Nothing Nothing
-            GLFW.makeContextCurrent win
-            return win
-
-withGL :: Maybe WindowConf -> Maybe GLFW.ErrorCallback -> (GLFW.Window -> IO a) -> IO a
-withGL wc ec action = withErrorCallback ec . withGLFW . withNewWindow wc $ action
+data ContextFactoryRet a
+    = NewSharedContextRet ContextHandle
+    | ContextDoSyncRet a
+    | ContextFrameBufferSizeRet (Int, Int)
+    | OtherRet
 
 -- eof
