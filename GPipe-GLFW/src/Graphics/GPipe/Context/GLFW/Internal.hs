7f218eaa9cad1b31890b323f57f4a71e8938ced3
PLR <51248199+plredmond@users.noreply.github.com>
2015-07-03 23:32:34 -0700

    remove confusing infix `bracket` calls; readability

--- GPipe-GLFW/src/Graphics/GPipe/Context/GLFW/Internal.hs
+++ GPipe-GLFW/src/Graphics/GPipe/Context/GLFW/Internal.hs
@@ -43,27 +43,37 @@ defaultWindowConf = WindowConf 1024 768 "GLFW Window"
 -- set and unset the GLFW error callback, using a default if none is provided
 withErrorCallback :: Maybe ErrorCallback -> IO a -> IO a
 withErrorCallback customOnError =
-    GLFW.setErrorCallback (Just $ M.fromMaybe defaultOnError customOnError)
-    `Exc.bracket_`
-    GLFW.setErrorCallback Nothing 
+    Exc.bracket_
+        (GLFW.setErrorCallback $ Just onError)
+        (GLFW.setErrorCallback Nothing)
+    where
+        onError :: ErrorCallback
+        onError = M.fromMaybe defaultOnError customOnError
 
 -- init and terminate GLFW
 withGLFW :: IO a -> IO a
-withGLFW = GLFW.init `Exc.bracket_` GLFW.terminate
+withGLFW =
+    Exc.bracket_
+        GLFW.init
+        GLFW.terminate
 
 -- create and destroy a new window, as the current context, using any monitor
 withNewWindow :: Maybe WindowConf -> (Window -> IO a) -> IO a
 withNewWindow customWindowConf =
-    (M.fromMaybe noWindow <$> createWindowHuh)
-    `Exc.bracket`
-    GLFW.destroyWindow
+    Exc.bracket
+        createWindow
+        GLFW.destroyWindow
     where
-        noWindow = error "Couldn't create a window"
         WindowConf w h t = M.fromMaybe defaultWindowConf customWindowConf
+        createWindowHuh :: IO (Maybe Window)
         createWindowHuh = do
             win <- GLFW.createWindow w h t Nothing Nothing
             GLFW.makeContextCurrent win
             return win
+        noWindow :: Window
+        noWindow = error "Couldn't create a window"
+        createWindow :: IO Window
+        createWindow = M.fromMaybe noWindow <$> createWindowHuh
 
 withGL :: Maybe WindowConf -> Maybe ErrorCallback -> (Window -> IO a) -> IO a
 withGL wc ec action = withErrorCallback ec . withGLFW . withNewWindow wc $ action
