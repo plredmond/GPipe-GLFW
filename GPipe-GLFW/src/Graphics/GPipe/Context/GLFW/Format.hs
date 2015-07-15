0a4a09a6bf837eedc87320e8ac6233ed5e0c6912
PLR <51248199+plredmond@users.noreply.github.com>
2015-07-15 14:19:00 -0700

    updated to use GPipe's contextBits function

--- GPipe-GLFW/src/Graphics/GPipe/Context/GLFW/Format.hs
+++ GPipe-GLFW/src/Graphics/GPipe/Context/GLFW/Format.hs
@@ -6,33 +6,26 @@ module Graphics.GPipe.Context.GLFW.Format
 
 -- qualified
 import qualified "GLFW-b" Graphics.UI.GLFW as GLFW
-import Graphics.GPipe.Format as F
+import qualified Graphics.GPipe.Format as F
 
 ------------------------------------------------------------------------------
 -- Top-level
 
-toHints :: ContextFormat c ds -> [GLFW.WindowHint]
-toHints F.ContextFormatNone = [] -- TODO: should this hint *not* to have anything? or should this be a "sane default"
-toHints (F.ContextFormatDepth d) =
-    [ GLFW.WindowHint'DepthBits $ F.depthBits d ]
-toHints (F.ContextFormatStencil s) =
-    [ GLFW.WindowHint'StencilBits $ F.stencilBits s ]
-toHints (F.ContextFormatDepthStencil ds) =
-    let (d, s) = F.depthStencilBits ds in
-    [ GLFW.WindowHint'DepthBits d
-    , GLFW.WindowHint'StencilBits s ]
-toHints (F.ContextFormatColor c) =
-    [ GLFW.WindowHint'RedBits $ F.redBits c
-    , GLFW.WindowHint'GreenBits $ F.greenBits c
-    , GLFW.WindowHint'BlueBits $ F.blueBits c ]
-toHints (F.ContextFormatColorDepth c d) =
-    toHints (F.ContextFormatColor c) ++
-    toHints (F.ContextFormatDepth d)
-toHints (F.ContextFormatColorStencil c s) =
-    toHints (F.ContextFormatColor c) ++
-    toHints (F.ContextFormatStencil s)
-toHints (F.ContextFormatColorDepthStencil c ds) =
-    toHints (F.ContextFormatColor c) ++
-    toHints (F.ContextFormatDepthStencil ds)
+toHints :: F.ContextFormat c ds -> [GLFW.WindowHint]
+toHints fmt =
+    [ GLFW.WindowHint'sRGBCapable sRGB
+    , GLFW.WindowHint'Visible visible
+    , GLFW.WindowHint'RedBits red
+    , GLFW.WindowHint'GreenBits green
+    , GLFW.WindowHint'BlueBits blue
+    , GLFW.WindowHint'AlphaBits 0
+    , GLFW.WindowHint'DepthBits depth
+    , GLFW.WindowHint'StencilBits stencil
+    ]
+    where
+        ((red, green, blue, sRGB), depth, stencil) = F.contextBits fmt
+        visible = case fmt of
+            F.ContextFormatNone -> False
+            _ -> True
 
 -- eof
