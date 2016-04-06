611043c113c4ce6084b2072058d83000721b74a8
PLR <51248199+plredmond@users.noreply.github.com>
2016-04-05 18:26:42 -0700

    break mainloop into several pieces according to purpose and monad/transformer

--- GPipe-GLFW/test/Spec.hs
+++ GPipe-GLFW/test/Spec.hs
@@ -1,88 +1,136 @@
---import Control.Monad.IO.Class (MonadIO)
-import Control.Concurrent (threadDelay)
-import Control.Monad.Trans.Class (lift)
+{-# LANGUAGE Rank2Types #-}
+
+import Control.Monad.Exception (MonadException)
+import Control.Monad.IO.Class (MonadIO)
 import Data.Traversable (forM)
 
 import Graphics.GPipe
 import qualified Graphics.GPipe.Context.GLFW as GLFW
 
-meshes :: [[(V3 Float, V3 Float)]]
-meshes =
-    [ zip (repeat $ V3 1 0 0) -- red x axis
-        [V3 (-1) 0 0, V3 1 0 0, V3 0.8 (-0.1) 0, V3 0.8 0 0, V3 1 (-0.1) 0]
-    , zip (repeat $ V3 0 1 0) -- green y axis
-        [V3 0 (-1) 0, V3 0 1 0, V3 0 0.8 (-0.1), V3 0 0.9 (-0.05), V3 0 1 (-0.1)]
-    , zip (repeat $ V3 0 0 1) -- blue z axis
-        [V3 0 0 (-1), V3 0 0 1, V3 (-0.1) 0 0.8, V3 (-0.1) 0 1]
-    , zip (repeat $ V3 1 1 1) -- white plane mesh
-        [V3 (-2) (-0.1) (-2), V3 (-2) (-0.1) 2, V3 2 (-0.1) 2, V3 2 (-0.1) (-2), V3 (-2) (-0.1) (-2)]
-    ]
+xAxis :: [(V3 Float, V3 Float)]
+xAxis = zip (repeat $ V3 1 0 0) -- red
+    [V3 (-1) 0 0, V3 1 0 0, V3 0.8 (-0.1) 0, V3 0.8 0 0, V3 1 (-0.1) 0]
+
+yAxis :: [(V3 Float, V3 Float)]
+yAxis = zip (repeat $ V3 0 1 0) -- green
+    [V3 0 (-1) 0, V3 0 1 0, V3 0 0.8 (-0.1), V3 0 0.9 (-0.05), V3 0 1 (-0.1)]
+
+zAxis :: [(V3 Float, V3 Float)]
+zAxis = zip (repeat $ V3 0 0 1) -- blue
+    [V3 0 0 (-1), V3 0 0 1, V3 (-0.1) 0 0.8, V3 (-0.1) 0 1]
+
+plane :: [(V3 Float, V3 Float)]
+plane = zip (repeat $ V3 1 1 1) -- white
+    [V3 (-2) (-0.1) (-2), V3 (-2) (-0.1) 2, V3 2 (-0.1) 2, V3 2 (-0.1) (-2), V3 (-2) (-0.1) (-2)]
 
 main :: IO ()
-main = do
-    basicTest
+main = basicTest (3 * 60)
 
-basicTest :: IO ()
-basicTest = runContextT GLFW.newContext (ContextFormatColorDepth RGB8 Depth16)
-    (mainloop (3 * 60) Nothing)
+basicTest :: Int -> IO ()
+basicTest frameCount = runContextT GLFW.newContext
+        (ContextFormatColorDepth RGB8 Depth16)
+        (initRenderContext [xAxis,yAxis,zAxis,plane] >>= mainloop (frameCount, 0))
 
-data ShaderEnv = ShaderEnv
-    { extractPointsPA :: PrimitiveArray Lines (B3 Float, B3 Float)
+-- TODO: make tests
+--   * basic
+--   * split-thread load/render
+--   * render multiple windows
+
+data ShaderEnv os = ShaderEnv
+    { extractProjU :: (Buffer os (Uniform (V4 (B4 Float))), Int)
+    , extractLinePA :: PrimitiveArray Lines (B3 Float, B3 Float)
     , extractRastOpts :: (Side, ViewPort, DepthRange)
     }
 
-mainloop :: Int -> Maybe
-    ( ( [Buffer os (B3 Float, B3 Float)]
-      , Buffer os (Uniform (V4 (B4 Float)))
-      , CompiledShader os (ContextFormat RGBFloat Depth) ShaderEnv
-      )
-    , Int
-    ) -> ContextT w os (ContextFormat RGBFloat Depth) IO ()
-mainloop maxFrame Nothing = do -- ContextT monad transformer
+initBuffers
+    :: MonadIO m
+    => [[(V3 Float, V3 Float)]]
+    -> ContextT w os f m ([Buffer os (B3 Float, B3 Float)], Buffer os (Uniform (V4 (B4 Float))))
+initBuffers rawMeshes = do
     -- make mesh buffers
-    meshesB <- forM meshes $ \pts -> do
+    meshesB <- forM rawMeshes $ \pts -> do
         buf <- newBuffer $ length pts
         writeBuffer buf 0 pts
         return buf
     -- make projection matrix buffer
     projMatB <- newBuffer 1
-    -- make simple projection shader
-    projShader <- compileShader $ do -- Shader monad
-        proj <- getUniform (const (projMatB, 0))
-        pointsPS <- toPrimitiveStream extractPointsPA
-        -- project points and then rasterize them
-        lineFS <- rasterize extractRastOpts
-            . fmap (\(c, p) -> (proj !* point p, c)) $ pointsPS
-        -- write fragment depths and then draw them to the screen
-        let depth RasterizedInfo {rasterizedFragCoord = (V4 _ _ z _)} = z
-        drawContextColorDepth (const (ContextColorOption NoBlending $ pure True, DepthOption Less True))
-            . withRasterizedInfo (\fr inf -> (fr, depth inf)) $ lineFS -- write fragment depth
-    mainloop maxFrame $ Just ((meshesB, projMatB, projShader), 0)
-mainloop maxFrame resources@(Just ((meshesB, projMatB, projShader), curFrame))
-    | curFrame > maxFrame = return ()
-    | otherwise = do -- ContextT monad transformer
+    return (meshesB, projMatB)
+
+projectLines :: forall os. Shader os (ContextFormat RGBFloat Depth) (ShaderEnv os) (FragmentStream (V3 FFloat, FFloat))
+projectLines = do
+    projMat <- getUniform extractProjU
+    linePS <- toPrimitiveStream extractLinePA
+    -- project points
+    let projectedLinePS = (\(c, p) -> (projMat !* point p, c)) <$> linePS
+    lineFS <- rasterize extractRastOpts projectedLinePS
+    -- write fragment depths and return frags
+    return $ withRasterizedInfo (\fr inf -> (fr, depth inf)) lineFS
+    where
+        depth RasterizedInfo {rasterizedFragCoord = (V4 _ _ z _)} = z
+
+-- TODO: Candidate for removal; just "draws" the shader result w/o adding any other behavior
+initRenderContext
+    :: (MonadIO m, MonadException m)
+    => [[(V3 Float, V3 Float)]]
+    -> ContextT w os (ContextFormat RGBFloat Depth) m
+        ( [Buffer os (B3 Float, B3 Float)]
+        , Buffer os (Uniform (V4 (B4 Float)))
+        , CompiledShader os (ContextFormat RGBFloat Depth) (ShaderEnv os)
+        )
+initRenderContext rawMeshes = do
+    projShader <- compileShader (projectLines >>= drawContextColorDepth (const (ContextColorOption NoBlending $ pure True, DepthOption Less True)))
+    (meshesB, projMatB) <- initBuffers rawMeshes
+    return (meshesB, projMatB, projShader)
+
+computeProjMat :: Float -> V2 Int -> M44 Float
+computeProjMat frac (V2 w h) = camera2clip !*! world2camera !*! model2world
+    where
+        t = frac * 2 * pi
+        model2world = identity
+        world2camera = lookAt
+            -- eye: camera position in world coordinates
+            (V3 (3 * sin t) (0.5 + cos (t * 2)) (3 * cos t))
+            (V3 0 0.1 0) -- center: camera look-at target position in world coords
+            (V3 0 1 0) -- up: camera up-vector
+        camera2clip = perspective
+            (pi / 3) -- 60deg field of view "in y direction"
+            (fromIntegral w / fromIntegral h) -- aspect ratio
+            1 100 -- near and far clipping plane
+
+basicRenderer
+    :: V2 Int
+    -> ( [Buffer os (B3 Float, B3 Float)]
+       , Buffer os (Uniform (V4 (B4 Float)))
+       , CompiledShader os (ContextFormat RGBFloat Depth) (ShaderEnv os)
+       )
+    -> Render os (ContextFormat RGBFloat Depth) ()
+basicRenderer size (meshesB, projMatB, projShader) = do
+    clearContextColor 0.2 -- grey
+    clearContextDepth 1 -- far plane
+    meshPAs <- forM meshesB $ \mesh -> do
+        meshVA <- newVertexArray mesh
+        return $ toPrimitiveArray LineStrip meshVA
+    projShader $ ShaderEnv (projMatB, 0) (mconcat meshPAs) (Front, ViewPort 0 size, DepthRange 0 1)
+
+mainloop
+    ::  (MonadIO m, MonadException m)
+    => (Int, Int)
+    -> ( [Buffer os (B3 Float, B3 Float)]
+       , Buffer os (Uniform (V4 (B4 Float)))
+       , CompiledShader os (ContextFormat RGBFloat Depth) (ShaderEnv os)
+       )
+    -> ContextT w os (ContextFormat RGBFloat Depth) m ()
+mainloop frame resources@(_, projMatB, _)
+    | done frame = return ()
+    | otherwise = do
         -- compute a projection matrix & write it to the buffer
-        size@(V2 w h) <- getContextBuffersSize
-        let t = fromIntegral curFrame / fromIntegral maxFrame * 2 * pi
-            model2world = identity
-            world2camera = lookAt
-                -- eye: camera position in world coordinates
-                (V3 (3 * sin t) (0.5 + cos (t * 2)) (3 * cos t))
-                (V3 0 0.1 0) -- center: camera look-at target position in world coords
-                (V3 0 1 0) -- up: camera up-vector
-            camera2clip = perspective
-                (pi / 3) -- 60deg field of view "in y direction"
-                (fromIntegral w / fromIntegral h) -- aspect ratio
-                1 100 -- near and far clipping plane
-        writeBuffer projMatB 0 [camera2clip !*! world2camera !*! model2world]
+        size <- getContextBuffersSize
+        writeBuffer projMatB 0 [computeProjMat (frac frame) size]
         -- render the scene and then loop
-        render $ do -- Render monad
-            clearContextColor 0.2 -- grey
-            clearContextDepth 1 -- far plane
-            meshPAs <- forM meshesB $ \mesh -> do
-                meshVA <- newVertexArray mesh
-                return $ toPrimitiveArray LineStrip meshVA
-            projShader $ ShaderEnv (mconcat meshPAs) (Front, ViewPort 0 size, DepthRange 0 1)
+        render $ basicRenderer size resources
         swapContextBuffers
-        lift . threadDelay . round $ 1e6 / (60 :: Float) -- fps <= 60
-        mainloop maxFrame (fmap (+1) <$> resources)
+        mainloop (next frame) resources
+    where
+        done (maxFrame, curFrame) = curFrame > maxFrame
+        frac (maxFrame, curFrame) = fromIntegral curFrame / fromIntegral maxFrame
+        next = fmap (+1)
