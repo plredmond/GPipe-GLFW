8a87f5d5a52848c2b3e5c4d4dcbfb900dc556987
PLR <51248199+plredmond@users.noreply.github.com>
2021-06-24 20:47:36 -0700

    unpin

--- default.nix
+++ default.nix
@@ -1,26 +1,15 @@
 { mkEnv ? null
 , target ? "GPipe-GLFW"
 , config ? { allowBroken = true; }
+, nixpkgs ? import <nixpkgs> { inherit config; }
 , ...
 }:
 let
-  nixpkgs = import
-    (builtins.fetchTarball {
-      # latest https://github.com/NixOS/nixpkgs/commits/nixos-20.09 as of Sun 01 Nov 2020 10:25:16 PM UTC
-      url = "https://github.com/NixOS/nixpkgs/archive/edb26126d98bc696f4f3e206583faa65d3d6e818.tar.gz";
-      sha256 = "1cl4ka4kk7kh3bl78g06dhiidazf65q8miyzaxi9930d6gwyzkci";
-    })
-    { inherit config; };
   # package set for haskell compiler version
   haskellCompilerPackages = nixpkgs.haskellPackages; # don't care about compiler version
   # override package set to inject project components
   haskellPackages = haskellCompilerPackages.override
     (old: {
-      all-cabal-hashes = nixpkgs.fetchurl {
-        # latest https://github.com/commercialhaskell/all-cabal-hashes/commits/hackage as of Sun 01 Nov 2020 10:26:50 PM UTC
-        url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/ce6d987d56e5a3f2af4a41a417405e43db36e337.tar.gz";
-        sha256 = "17i6hz4063xhxqzdddp38clhm9sccsd9y4wykz65hikhaxhpnwlh";
-      };
       overrides = self: super: with nixpkgs.haskell.lib; rec {
         # declare each of the packages in this project
         GPipe = overrideCabal (self.callHackage "GPipe" "2.2.5" { }) {
